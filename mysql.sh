09de17c52c38f885a4ae65535c158fabb3b9d883
McKenzie Flavius <mckenzie@McKenzies-MacBook-Pro.local>
2020-05-22 01:06:58 +0100

    new syntax

--- mysql.sh
+++ mysql.sh
@@ -17,8 +17,8 @@ echo $passwordProduction
 
 #mysql -u root -e "SET PASSWORD FOR user@'127.0.0.1' = PASSWORD(‘(#ogg^qqvmBmtChlC&p.’);"
 
-mysql -u root -e "SET PASSWORD FOR 'user'@'127.0.0.1' IDENTIFIED BY PASSWORD('$passwordUser');"
+mysql -u root -e "SET PASSWORD FOR 'user'@'127.0.0.1' = PASSWORD('$passwordUser');"
 
-mysql -u root -e "SET PASSWORD FOR 'development'@'localhost' IDENTIFIED BY PASSWORD('$passwordDevelopment');"
-mysql -u root -e "SET PASSWORD FOR 'staging'@'localhost' IDENTIFIED BY PASSWORD('$passwordStaging');"
-mysql -u root -e "SET PASSWORD FOR 'production'@'localhost' IDENTIFIED BY PASSWORD('$passwordProduction');"
+mysql -u root -e "SET PASSWORD FOR 'development'@'localhost' = PASSWORD('$passwordDevelopment');"
+mysql -u root -e "SET PASSWORD FOR 'staging'@'localhost' = PASSWORD('$passwordStaging');"
+mysql -u root -e "SET PASSWORD FOR 'production'@'localhost' = PASSWORD('$passwordProduction');"
