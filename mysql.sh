f2c0a71bd9a0140bfdbfb79a32a5ff5d87f63356
McKenzie Flavius <mckenzie@McKenzies-MacBook-Pro.local>
2020-05-22 01:46:03 +0100

    Added new password intensity and possibility, script working as of this push

--- mysql.sh
+++ mysql.sh
@@ -2,7 +2,7 @@
 mysql.server start
 
 random-string(){
-    cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1
+    cat /dev/urandom | tr -dc 'a-zA-Z0-9~!@#$%^&()_-+={}[]/<>,;?:|' | fold -w 32 | head -n 1
 }
 
 passwordUser=$(random-string)
@@ -10,15 +10,14 @@ passwordDevelopment=$(random-string)
 passwordStaging=$(random-string)
 passwordProduction=$(random-string)
 
-echo $passwordUser
-echo $passwordDevelopment
-echo $passwordStaging
-echo $passwordProduction
-
 #mysql -u root -e "SET PASSWORD FOR user@'127.0.0.1' = PASSWORD(‘(#ogg^qqvmBmtChlC&p.’);"
 
 mysql -u root -e "SET PASSWORD FOR 'user'@'127.0.0.1' = PASSWORD('$passwordUser');"
-
 mysql -u root -e "SET PASSWORD FOR 'development'@'localhost' = PASSWORD('$passwordDevelopment');"
 mysql -u root -e "SET PASSWORD FOR 'staging'@'localhost' = PASSWORD('$passwordStaging');"
 mysql -u root -e "SET PASSWORD FOR 'production'@'localhost' = PASSWORD('$passwordProduction');"
+
+echo "$passwordUser - User"
+echo "$passwordDevelopment - Development"
+echo "$passwordStaging - Staging"
+echo "$passwordProduction - Production"
